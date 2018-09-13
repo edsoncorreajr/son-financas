0b14a1b43cefdc25ee8ddc4f02ea9d6c4845ab4e
edsoncorreajr <edsoncorreajr@gmail.com>
2018-09-12 21:47:29 -0300

    Implementado Exclusão de categorias de custo

--- public/index.php
+++ public/index.php
@@ -71,19 +71,87 @@ $app
     },
     'category-costs.new'
 )
-->post(
-    '/category-costs/store',
-    function (ServerRequestInterface $request) use ($app) {
-        // cadastro de categorias
+    ->get(
+        '/category-costs/{id}/edit',
+        function (ServerRequestInterface $request) use ($app) {
+            $view = $app->service('view.renderer');
+            $id = $request->getAttribute('id');
+            $category = CategoryCost::findOrFail($id);
+
+            return $view->render(
+                'category-costs/edit.html.twig',
+                [
+                    'categoria' => $category
+                ]
+            );
+        },
+        'category-costs.edit'
+    )
+    ->get(
+        '/category-costs/{id}/show',
+        function (ServerRequestInterface $request) use ($app) {
+            $view = $app->service('view.renderer');
+            $id = $request->getAttribute('id');
+            $category = CategoryCost::findOrFail($id);
+
+            return $view->render(
+                'category-costs/show.html.twig',
+                [
+                    'categoria' => $category
+                ]
+            );
+        },
+        'category-costs.show'
+    )
+    ->post(
+        '/category-costs/{id}/update',
+        function (ServerRequestInterface $request) use ($app) {
+            $view = $app->service('view.renderer');
+            $id = $request->getAttribute('id');
+
+            /**
+             * @var CategoryCost $category
+             */
+            $category = CategoryCost::findOrFail($id);
+            $data = $request->getParsedBody();
+
+            $category->fill($data); // atualiza a consulta com o dado form - atribuição massiva
+
+            $category->save();
+
+            return $app->route('category-costs.list');
+        },
+        'category-costs.update'
+    )
+    ->get(
+        '/category-costs/{id}/delete',
+        function (ServerRequestInterface $request) use ($app) {
+            $view = $app->service('view.renderer');
+            $id = $request->getAttribute('id');
+
+            /**
+             * @var CategoryCost $category
+             */
+            $category = CategoryCost::findOrFail($id);
+            $category->delete();
+
+            return $app->route('category-costs.list');
+        },
+        'category-costs.delete'
+    )
+    ->post(
+        '/category-costs/store',
+        function (ServerRequestInterface $request) use ($app) {
+            // cadastro de categorias
         
 
-        $data = $request->getParsedBody();
-        CategoryCost::create($data);
-        //após inserir redireciona para a pagina de lista categorias
-        return $app->route('category-costs.list');
-    },
-    'category-costs.store'
-);
+            $data = $request->getParsedBody();
+            CategoryCost::create($data);
+            //após inserir redireciona para a pagina de lista categorias
+            return $app->route('category-costs.list');
+        },
+        'category-costs.store'
+    );
 
 /* $app->get('/home/{name}', function (ServerRequestInterface $request) {
     $response = new Response();
