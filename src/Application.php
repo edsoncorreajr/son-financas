551f6f424096a211b41968c6c534b128121fb761
edsoncorreajr <edsoncorreajr@gmail.com>
2018-09-18 02:27:08 -0300

    Criando Estrutura de barreira para as rotas administrativas

--- src/Application.php
+++ src/Application.php
@@ -13,6 +13,7 @@ use Zend\Diactoros\Response\RedirectResponse;
 class Application
 {
     private $serviceContainer;
+    private $befores = [];
 
     /**
      * Application constructor
@@ -63,34 +64,51 @@ class Application
         $routing->post($name, $path, $action);
         return $this;
     }
-    public function redirect($path): RedirectResponse 
+    public function redirect($path): ResponseInterface
     {
         return new RedirectResponse($path);
     }
 
-    protected function emitResponse(ResponseInterface $response)
+    protected function emitResponse(ResponseInterface $response): void
     {
-        /** 
+        /**
          * @var SapiEmitter $emitter
          */
         $emitter = new SapiEmitter();
         $emitter->emit($response);
-        
     }
 
-    /** 
+    /**
      * cria função para realizar o roteamento
      * @var string $name : recebe o nome para a rota
      * @var array $params : padrão null, recebe os parametros atribuidos para a rota
      * @return RedirectResponse
      */
-    public function route(string $name, array $params = []){
+    public function route(string $name, array $params = []): ResponseInterface
+    {
         $generator = $this->service('routing.generator');
         $path = $generator->generate($name, $params);
         return $this->redirect($path);
     }
 
-    public function start()
+    public function before(callable $callback): Application
+    {
+        array_push($this->befores, $callback);
+        return $this;
+    }
+
+    protected function runBefores(): ?ResponseInterface
+    {
+        foreach ($this->befores as $callback) {
+            $result = $callback($this->service(RequestInterface::class));
+            if ($result instanceof ResponseInterface) {
+                return $result;
+            }
+        }
+        return null;
+    }
+
+    public function start(): void
     {
         /* pega a rota enviada pelo usuario  */
         $route = $this->service('route');
@@ -107,16 +125,27 @@ class Application
             $request = $request->withAttribute($key, $value);
         }
 
+        $result = $this->runBefores();
+        if($result){
+            $this->emitResponse($result);
+            return;
+        }
+
         // captura a ação da rota chamada
         $callable = $route->handler;
 
         // response chama a ação da rota
-        /** 
-         * @var ResponseInterface $request 
+        /**
+         * @var ResponseInterface $request
          */
         $response = $callable($request);
 
         // emite a resposta
         $this->emitResponse($response);
     }
+    /**
+     * Lógica - criar função que realiza análise sobre  rota e compara com usuario
+     * a função retorna uma resposta ou redirecionamento
+     * trabalha conceito de Midleware - equivalente execução em fila
+     */
 }
